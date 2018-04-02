f8b25205acbac5ec3c295627d20726b26db63b29
Lizy321 <32933043+Lizy321@users.noreply.github.com>
2018-04-02 13:09:11 +0200

    Add files via upload

--- analyze_network.py
+++ analyze_network.py
@@ -41,23 +41,63 @@ def construct_subgraph(a2q,c2a,c2q):
 
 sub_a2q, sub_c2a, sub_c2q = construct_subgraph(G_a2q,G_c2a,G_c2q)
 
+# np_a2q = DataFrame(list(sub_a2q.edges)).as_matrix()
+# np.savetxt('/Users/lizy/Downloads/Q3/Complex_Network/project/a2q_subgraph.txt',np_a2q)
+# np_c2a = DataFrame(list(sub_a2q.edges)).as_matrix()
+# np.savetxt('/Users/lizy/Downloads/Q3/Complex_Network/project/c2a_subgraph.txt',np_c2a)
+# np_c2q = DataFrame(list(sub_a2q.edges)).as_matrix()
+# np.savetxt('/Users/lizy/Downloads/Q3/Complex_Network/project/c2q_subgraph.txt',np_c2q)
+
+
+def df_cen(data,str):
+    return DataFrame({'node': list(data.keys()), str: list(data.values())})
 
 # compute the centrality in each layer
 def centrality(G):
-
+    time1 = time.time()
     degree = nx.degree_centrality(G)
+
+    df_degree = df_cen(degree,'degree')
+
     print("Degree computed!")
+
+    time2 = time.time()
+    print(time2-time1)
+
     betweenness = nx.betweenness_centrality(G)
+    df_betweenness = df_cen(betweenness, 'betweenness')
     print("Betweenness computed!")
+
+    time3 = time.time()
+    print(time3 - time2)
+
     closeness = nx.closeness_centrality(G)
+    df_closeness = df_cen(closeness, 'closeness')
     print("Closeness computed!")
+
+    time4 = time.time()
+    print(time4 - time3)
+
     communicability = nx.communicability(G)
+    df_communicability = df_cen(communicability, 'communicatibility')
     print("Communicability computed!")
-    df = DataFrame({'degree':degree,'betweenness':betweenness,"closeness":closeness,"communicability":communicability})
-    return df
+
+    time5 = time.time()
+    print(time5 - time4)
+
+    centralities = pd.merge(df_degree, df_betweenness, df_closeness, df_communicability, on=['node'], how='outer')
+
+    return centralities
 
 cent_a2q = centrality(sub_a2q)
 cent_c2q = centrality(sub_c2q)
 cent_c2a = centrality(sub_c2a)
 
-# Correlation between these centralities can be implmented in the following 
+def write_file(data,activity):
+    np.savetxt('/Users/lizy/Downloads/Q3/Complex_Network/project/'+ activity +'_centrality.txt',data.as_matrix())
+
+write_file(cent_a2q,'a2q')
+write_file(cent_c2q,'c2q')
+write_file(cent_c2a,'c2a')
+
+# Correlation between these centralities can be implmented in the following
